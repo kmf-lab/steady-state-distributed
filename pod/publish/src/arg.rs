24bc30fd55220f102f11dcdcf4f31a05a5bd8acb
nathantippy <nathan@tippy.name>
2025-04-28 16:51:17 -0500

    faster

--- pod/publish/src/arg.rs
+++ pod/publish/src/arg.rs
@@ -2,8 +2,8 @@ use clap::Parser;
 
 #[derive(Parser, Debug, PartialEq, Clone)]
 pub(crate) struct MainArg {
-    #[arg(short = 'r', long = "rate", default_value = "1000")]
+    #[arg(short = 'r', long = "rate", default_value = "100")]
     pub(crate) rate_ms: u64,
-    #[arg(short = 'b', long = "beats", default_value = "600")]
+    #[arg(short = 'b', long = "beats", default_value = "6000")]
     pub(crate) beats: u64,
 }
